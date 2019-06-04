bf835b576a4f5e4da6084ee27175c97ea37a341b
Hem Nalini Morzaria-Luna <hmorzaria@hotmail.com>
2019-06-04 12:24:15 +0200

    Create docker.sh
    First commit

---
+++ docker.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo "Install a few prerequisite packages which let apt use packages over HTTPS"
+
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+
+echo "Add the GPG key for the official Docker repository to your system"
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+echo "Add the Docker repository to APT sources"
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+
+echo "Update the package database with the Docker packages from the newly added repo"
+
+sudo apt update
+
+echo "Install from the Docker repo instead of the default Ubuntu repo"
+
+apt-cache policy docker-ce
+
+echo "install Docker"
+
+sudo apt install -y docker-ce
+
+sudo systemctl status docker
+
+sudo usermod -a -G docker $USER
+
+echo "After the script completes close SSH session and reconnect"
+
+if [ -d $HOME/bin ]; then
+PATH=$PATH:$HOME/bin
+fi
