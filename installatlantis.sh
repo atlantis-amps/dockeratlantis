22123299b51ef95a4cede867f145ebc299031ed4
Hem Nalini Morzaria-Luna <hmorzaria@hotmail.com>
2019-06-04 16:04:36 +0200

    Update installatlantis.sh

--- installatlantis.sh
+++ installatlantis.sh
@@ -1,15 +1,14 @@
-#Install Atlantis
-#### Get code from CSIRO SVN, if need to change version use co -5468 for example
-       svn co https://svnserv.csiro.au/svn/ext/atlantis/Atlantis/trunk --username <username> --password <password> --quiet
-       
-##Rebuild recompile via MAKE: 
-        cd trunk/atlantis
-        aclocal
-        autoheader
-        autoconf
-        automake -a
-        ./configure
-        sudo make CFLAGS='-Wno-misleading-indentation -Wno-format -Wno-implicit-fallthrough'
-        sudo make -d install
-        cd atlantismain
+#!/bin/bash
+
+echo "Install Atlantis"
+
+cd trunk/atlantis; aclocal; autoheader; autoconf; automake -a; ./configure; sudo make CFLAGS='-Wno-misleading-indentation -Wno-format -Wno-implicit-fallthrough'; sudo make -d install; cd atlantismain
+
+
+if [ -d $HOME/bin ]; then
+PATH=$PATH:$HOME/bin
+fi
+
+        
+
   
